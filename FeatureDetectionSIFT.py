cc726564d4933ca0bd11c9e5445b0855b1d316ed
Varun Rao <varun21exps@gmail.com>
2017-09-09 17:32:41 +0530

    Tried FeatureDetectionSIFT.py

---
+++ FeatureDetectionSIFT.py
@@ -0,0 +1,18 @@
+# FeatureDetectionSIFT.py for trying out OpenCV Feature Detection using SIFT
+
+import cv2
+import numpy as np
+img = cv2.imread('training-images/Digits/1/Right_Hand/Normal/2.png')
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+sift = cv2.xfeatures2d.SIFT_create()
+kp = sift.detect(gray,None)
+img=cv2.drawKeypoints(gray,kp,img)
+cv2.imwrite('sift_keypoints.jpg',img)
+
+sift = cv2.xfeatures2d.SIFT_create()
+kp, des = sift.detectAndCompute(gray,None)
+
+print("Size of numpy array:"+str(des.shape))
+import matplotlib.pyplot as plt
+plt.hist(des, bins='auto')
+plt.show()
