3d9cc74bc9ad78aaeb3eddae0bd57a2e28a9c841
Varun Rao <varun21exps@gmail.com>
2017-10-19 01:13:10 +0530

    Saved keras model on file, no need to run training everytime before evaluating. Tried on 5 samples, all successfully classified

---
+++ LoadSavedModel.py
@@ -0,0 +1,28 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.models import model_from_json
+import pandas as pd
+import numpy as np
+
+# Loading the data
+dataframe = pd.read_csv("test_data.csv", header=None)
+dataset = dataframe.values
+X = dataset[:,0:10].astype(float)
+
+class_array=["1","2","3","4","5"]
+#For Keras Model
+print("Getting the saved model.")
+json_file = open('MLModels/KerasModel.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+loaded_model.load_weights("MLModels/KerasModel.h5")
+print("Loaded model from disk")
+
+print("Compiling the loaded model.")
+# evaluate loaded model on test data
+loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+predicted_digit=loaded_model.predict(X)
+for x in predicted_digit:
+    print("The predicted digit is:"+class_array[np.argmax(x)])
