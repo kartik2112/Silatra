3d31293f2c1375e373e0e590d166df60ac37e57a
Tejas <tejas.dastane@gmail.com>
2017-12-30 21:21:49 +0530

    Function call to segment skin from image

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import cv2
 from skin_detector import predict_skin_pixels
+from numpy import hstack
 
 img_file = input('Image: ')
 only_skin_img = predict_skin_pixels(img_file)
-cv2.imshow('Segmented image', only_skin_img)
-cv2.waitKey(10000)
+cv2.imshow('Result', hstack([only_skin_img]))
+cv2.waitKey(100000)
 cv2.destroyAllWindows()
