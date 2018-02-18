de6c97e9189f55c4f9160aa166fc253f501ddb89
Kartik Shenoy <kartik.shenoyy@gmail.com>
2018-02-18 21:10:54 +0530

    Grid KNN Model Saved

--- Jugaad/detectSign_SK.py
+++ Jugaad/detectSign_SK.py
@@ -6,14 +6,10 @@ import pandas as pd
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import confusion_matrix
 from sklearn.neighbors import KNeighborsClassifier
+import pickle
 
-data = pd.read_csv('new_data.csv')
-
-X = data[['f'+str(i) for i in range(400)]].values
-Y = data['label'].values
-
-classifier = KNeighborsClassifier(n_neighbors=3)
-classifier.fit(X, Y)
+classifier = pickle.load(open('KNNModelDump.sav','rb'))
+print("Loaded KNN Model")
 
 grid = (20,20)
 
