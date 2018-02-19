c001d17379cf7783d92e7359ca90d4891f3fb596
Varun Rao <varun21exps@gmail.com>
2018-02-19 23:41:50 +0530

    Good Morning automaton coded.

--- Gesture recognition/gesture_classify.py
+++ Gesture recognition/gesture_classify.py
@@ -1,19 +1,48 @@
 def ATBRecognizer(sequence):
-    state=0
-    finalStateReached=False
+    state = 0
+    finalStateReached = False
     for part in sequence:
         if state==0 and part[0]=="None":
-            state=0
+            state = 0
         elif state==0 and part[0]=="ThumbsUp":
-            state=1
-            finalStateReached=True
+            state = 1
+            finalStateReached = True
         elif state==1:
             if part[0]=="ThumbsUp" or part[0]=="None":
-                state=1
+                state = 1
     return finalStateReached
 
-
-
+def GMRecognizer(sequence):
+    state = 0
+    finalStateReached = False
+    for part in sequence:
+        if state==0:
+            if part[0]=="ThumbsUp":
+                state = 1
+        if state == 1:
+            if part[0]=="ThumbsUp":
+                state = 1
+            elif part[1]=="Down":
+                state = 2
+        if state == 2:
+            if part[1]=="Down":
+                state = 2
+            elif part[0]=="Cup_Closed":
+                state = 3
+        if state == 3:
+            if part[0]=="Cup_Closed":
+                state = 3
+            elif part[1]=="Up":
+                state = 4
+        if state == 4:
+            if part[0]=="Cup_Open":
+                state = 5
+                finalStateReached = True
+                break
+            elif part[1]=="Up":
+                state = 4
+    return finalStateReached
+    
 def recognize(sequence):
     gesture=""
     gesture_recognized=False
