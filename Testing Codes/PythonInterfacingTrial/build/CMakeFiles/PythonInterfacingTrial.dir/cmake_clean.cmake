0904c80974ce95fd2b0e68a1679708bde35d5419
Kartik Shenoy <kartik.shenoyy@gmail.com>
2018-03-12 23:13:45 +0530

    Repository reorganized

---
+++ Testing Codes/PythonInterfacingTrial/build/CMakeFiles/PythonInterfacingTrial.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PythonInterfacingTrial.dir/c++script.cpp.o"
+  "CMakeFiles/PythonInterfacingTrial.dir/cv_cpp_py_interface.cpp.o"
+  "PythonInterfacingTrial.pdb"
+  "PythonInterfacingTrial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PythonInterfacingTrial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
